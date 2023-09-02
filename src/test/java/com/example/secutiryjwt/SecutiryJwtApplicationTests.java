34e3c6636e18302e37622e1421814d2348d700f7
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-09-02 15:32:43 +0100

    security

---
+++ src/test/java/com/example/secutiryjwt/SecutiryJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secutiryjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecutiryJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
