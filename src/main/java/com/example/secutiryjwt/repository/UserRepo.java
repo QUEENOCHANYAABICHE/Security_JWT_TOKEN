fefdbfab0103aef1abc8004ee1655008022fffe7
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-09-02 20:55:36 +0100

    update3

--- src/main/java/com/example/secutiryjwt/repository/UserRepo.java
+++ src/main/java/com/example/secutiryjwt/repository/UserRepo.java
@@ -1,4 +1,10 @@
 package com.example.secutiryjwt.repository;
 
-public class UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
