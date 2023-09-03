802157aa189623365eb05bd33ee46cc5733cb998
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-09-03 13:17:41 +0100

    update4

--- src/main/java/com/example/secutiryjwt/config/JwtService.java
+++ src/main/java/com/example/secutiryjwt/config/JwtService.java
@@ -1,7 +1,40 @@
 package com.example.secutiryjwt.config;
 
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "Kn1Nxe7qI58zqviJYl2ewRNua41UA6/MMsMyvyx4+Vlby6Et+Q+z3fyNtTbMRp2D\n";
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+       return Jwts.parserBuilder()
+               .setSigningKey(getSignInKey())
+               .build()
+               .parseClaimsJws(token)
+               .getBody();
+
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
 }
