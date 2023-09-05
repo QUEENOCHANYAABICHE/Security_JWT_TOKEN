672d91e07f3293408bd5fca74a9fe14d40a1ffb1
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-09-05 09:48:25 +0100

    update6

--- src/main/java/com/example/secutiryjwt/config/auth/AuthenticationService.java
+++ src/main/java/com/example/secutiryjwt/config/auth/AuthenticationService.java
@@ -1,13 +1,36 @@
 package com.example.secutiryjwt.config.auth;
 
+import com.example.secutiryjwt.config.JwtService;
+import com.example.secutiryjwt.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import user.Role;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepo repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
     public AuthenticationResponse register(RegisterRequest request) {
-     return null;
+     var user = User.builder()
+             .firstname(request.getFirstname())
+             .lastname(request.getLastname())
+             .email(request.getEmail())
+             .password(passwordEncoder.encode(request.getPassword()))
+             .role(Role.USER)
+             .build();
+           repository.save(user);
+           var jwtToken = jwtService.generateToken(user);
+           return AuthenticationResponse.builder()
+                   .token(jwtToken)
+                   .build();
+
+
+        return null;
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
